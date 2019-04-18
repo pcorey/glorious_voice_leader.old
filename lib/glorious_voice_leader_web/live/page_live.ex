792ad3a4bb3f1747c00585ed40272e51a240a6eb
Pete Corey <petecorey@gmail.com>
2019-04-18 08:49:31 -0600

    Added clear/remove

--- lib/glorious_voice_leader_web/live/page_live.ex
+++ lib/glorious_voice_leader_web/live/page_live.ex
@@ -234,4 +234,92 @@ defmodule GVLWeb.PageLive do
     |> :erlang.term_to_binary()
     |> Base.encode64()
   end
+
+  def handle_event("clear_fretboard", input, socket) do
+    {index, _} = Integer.parse(input)
+
+    previous =
+      if index == 0 do
+        [nil, nil, nil, nil, nil, nil]
+      else
+        Enum.at(socket.assigns.chords, index - 1).playing
+      end
+
+    chords =
+      socket.assigns.chords
+      |> List.update_at(index, fn chord ->
+        playing = [nil, nil, nil, nil, nil, nil]
+
+        %{
+          chord
+          | playing: playing,
+            heatmap: Chord.Heatmap.generate(chord.root, chord.quality, playing, previous)
+        }
+      end)
+
+    chords =
+      if index == length(chords) - 1 do
+        chords
+      else
+        chords
+        |> List.update_at(
+          index + 1,
+          &%{
+            &1
+            | heatmap:
+                Chord.Heatmap.generate(
+                  &1.root,
+                  &1.quality,
+                  &1.playing,
+                  Enum.at(chords, index).playing
+                )
+          }
+        )
+      end
+
+    {:noreply,
+     socket
+     |> assign(:chords, chords)
+     |> assign(:token, update_token(chords))}
+  end
+
+  def handle_event("remove_fretboard", input, socket) do
+    {index, _} = Integer.parse(input)
+
+    previous =
+      if index == 0 do
+        [nil, nil, nil, nil, nil, nil]
+      else
+        Enum.at(socket.assigns.chords, index - 1).playing
+      end
+
+    chords =
+      socket.assigns.chords
+      |> List.delete_at(index)
+
+    chords =
+      if index == length(chords) do
+        chords
+      else
+        chords
+        |> List.update_at(
+          index,
+          &%{
+            &1
+            | heatmap:
+                Chord.Heatmap.generate(
+                  &1.root,
+                  &1.quality,
+                  &1.playing,
+                  Enum.at(chords, index - 1).playing
+                )
+          }
+        )
+      end
+
+    {:noreply,
+     socket
+     |> assign(:chords, chords)
+     |> assign(:token, update_token(chords))}
+  end
 end
