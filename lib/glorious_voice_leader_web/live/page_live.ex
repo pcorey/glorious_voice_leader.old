fb44de9d7a926d28619889c6e72cab4c5165d6dd
Pete Corey <petecorey@gmail.com>
2019-04-18 13:22:48 -0600

    Fixed half-diminished

--- lib/glorious_voice_leader_web/live/page_live.ex
+++ lib/glorious_voice_leader_web/live/page_live.ex
@@ -19,7 +19,7 @@ defmodule GVLWeb.PageLive do
   @quality_options %{
     "maj7" => [4, 3, 4, 1],
     "m7" => [3, 4, 3, 2],
-    "m7b5" => [3, 3, 3, 3],
+    "m7b5" => [3, 3, 4, 2],
     "7" => [4, 3, 3, 2]
   }
 
